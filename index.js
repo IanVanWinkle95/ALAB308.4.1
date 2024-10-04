35e5fbb12b750cd4e3f4790df68f4ad8e89d62bc
Ian Van Winkle <ianxvanwinkle@gmail.com>
2024-10-03 22:48:34 -0400

    commit 2

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// hello world
+// let CSV = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+// let cell = "";
+// let row = [];
+// let columns = 0;
+
+
+// for (let i = 0; i < CSV.length; i++) {
+//         if (CSV[i] === ",") {
+//         row.push(cell);
+//         cell = "";
+//     } else if (CSV[i] === "\n") {
+//         row.push(cell);
+//         console.log(...row);
+//         cell = "";
+//         row = [];
+//     } else {
+//         cell += CSV[i];
+//     }
+// }
+// if (cell) {
+//     row.push(cell);
+//     console.log(...row);
+// }
+
+// ---------------Part2-------------------
+
+// for (let i = 0; i < CSV.length; i++) {
+//     if (CSV[i] === ",") {
+//         columns++;
+//     } else if (CSV[i] === "\n") {
+//         columns++;
+//         break;
+//     }
+// }
+// console.log(columns);
+
+// // ---------------Part3------------------
+let row = [];
+let columns = 0;
+let columnNumber = 0;
+let obj = {};
+
+for (let row = 1; row < columns; row++) {
+    for (let columns = 0; columns < columnNumber; columns++) {
+
+    }
+
+}
